e00e1fa3014e861dcd44e4001e26d5262b3364df
Rutuja Patil <rspatil3562@gmail.com>
2022-11-04 12:25:26 +0530

    db updated

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,8 +2,6 @@ const mongoose=require("mongoose");
 
 const connect=()=>{return mongoose.connect(
   "mongodb+srv://Rutu:RutujaAtlas@cluster0.4soie.mongodb.net/grrendeck?retryWrites=true&w=majority"
-//  "mongodb://localhost:27017/register-api"
-//  "mongodb://127.0.0.1:27017/registerapigrrendeck"
 );
 };
 
